828264180dcf8ab6617cf10666d712a282d916e2
Satya Yadav <satya@example.com>
2026-01-18 21:01:05 +0530

    remove commented code

--- src/main/java/com/sagacious/repository/TaskRepository.java
+++ src/main/java/com/sagacious/repository/TaskRepository.java
@@ -1,7 +1,7 @@
-//package com.sagacious.repository;
-//
-//import com.sagacious.entity.Task;
-//import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-//
-//public interface TaskRepository extends ReactiveMongoRepository<Task, String> {
-//}
+package com.sagacious.repository;
+
+import com.sagacious.entity.Task;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+public interface TaskRepository extends ReactiveMongoRepository<Task, String> {
+}
